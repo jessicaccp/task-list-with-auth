fa660739cb41ead53e4c179d05487ffdfa429eab
Jessica Patricio <jessicacacau@gmail.com>
2025-04-25 10:43:45 -0300

    feat: add users app

---
+++ src/apps/users/serializers.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from rest_framework.serializers import ModelSerializer
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "username", "email", "password", "first_name", "last_name")
+
+    def create(self, validated_data):
+        user = User(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            password=validated_data["password"],
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"],
+        )
+        return user
